d343ad114809a2d87a8c55ce91a778a224c0bc3f
Ahmed <sultan_902002@yahoo.com>
2020-02-09 20:42:59 -0700

    add CORS and html title

--- recycling.py
+++ recycling.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_cors import CORS
 
 # instantiate the app
@@ -11,7 +11,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 
 if __name__ == '__main__':
