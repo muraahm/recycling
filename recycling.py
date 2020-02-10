4a8ebcea5e09ccd50de43054e52ec5904fa6c190
Ahmed <sultan_902002@yahoo.com>
2020-02-09 19:37:50 -0700

    add CORSE, activate debugger and render HTML template

--- recycling.py
+++ recycling.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
+
+# instantiate the app
 app = Flask(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
