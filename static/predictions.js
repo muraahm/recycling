cbac242091f334e7c4afba843b4f9ded048355be
Ahmed <sultan_902002@yahoo.com>
2020-02-12 13:19:20 -0700

    implement image capture function from the video stream

--- static/predictions.js
+++ static/predictions.js
@@ -1,9 +1,35 @@
-$(() => {
+let captureImage = function() {
+  let canvas = document.createElement("canvas");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d')
+        .drawImage(video, 0, 0, canvas.width, canvas.height);
+  let img = document.createElement("img");
+  img.src = canvas.toDataURL();
+  return img
+};
 
-  $('.predict').bind('click', function() {
-    console.log('hello')
-    // $('.initializing-container').hide()
-    // $('.recognition-container').show()
+
+$(() => {
+  $('.predict').bind('click', function () {
+
+    let picture = captureImage()
+
+    const predictRequest = $.ajax({
+      url: "/predict",
+      type: "POST",
+      data: picture,
+      contentType: false,
+      processData: false,
+    });
+
+    predictRequest.done(function (msg) {
+      console.log('success', msg)
+    });
+
+    predictRequest.fail(function (e) {
+      alert("Please try again");
+    });
   });
 
 })
