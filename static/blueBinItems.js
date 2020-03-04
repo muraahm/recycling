c16eea6d6eccb4b43f721166c4cccddeb4d94372
Ahmed <sultan_902002@yahoo.com>
2020-03-04 11:55:04 -0700

    add bin contents for classificatios

---
+++ static/blueBinItems.js
@@ -0,0 +1,15 @@
+export const blueBinItems = [
+  'can',
+  'water bottle',
+  'bottle',
+  'beer bottle',
+  'jar',
+  'envelope',
+  'cardboard box',
+  'box',
+  'newspaper',
+  'magazine',
+  'beer glass',
+  'plate',
+  'toilet paper'
+]
