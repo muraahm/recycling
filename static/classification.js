27829b039e69d586512fbdf0ea18c82bdf2bac77
Ahmed <sultan_902002@yahoo.com>
2020-03-04 11:54:14 -0700

    iterate through black and blue bin items and display a proper message to user

--- static/classification.js
+++ static/classification.js
@@ -1,36 +1,40 @@
-classifyItem = (item) => {
-  const yellowItemFound = find(yellowBinItems, yellowBinItem => item === yellowBinItem);
-  const redItemFound = find(redBinItems, redBinItem => item === redBinItem);
-
-  if(yellowItemFound){
-    this.displayButtons('yellow')
-  } else if(redItemFound) {
-    this.displayButtons('red')
-  } else {
-    this.displayButtons('none')
-  }
-}
+import {blueBinItems} from './blueBinItems.js';
+import {blackBinItems} from './blackBinItems.js';
 
 $(() => {
 
   $('.no').bind('click', () => {
     $('.classification-confirmation').hide()
-    $('#extra-details').text("Sorry! I'm not able to recognize the object in the picture, please try again.");
+    $('#extra-details').text("Sorry! I'm unable to recognize the object in the picture, please try again.");
     $('.recycling-classification').show()
   })
 
   $('.yes').bind('click', () => {
     const item = $('.data').text();
-    $('.classification-confirmation').hide()
+    const blueItemFound = blueBinItems.find(function(element) {
+      return element === item;
+    });
+    const blackItemFound = blackBinItems.find(function(element) {
+      return element === item;
+    });
 
+    if (blueItemFound) {
+      $('#extra-details').text(`It is recyclable! Throw it in the blue bin! 🎉`)
+    }
+    if (blackItemFound) {
+      $('#extra-details').text(`It is not recyclable 😢Throw it in the black bin.`)
+    }
+    if (!blackItemFound && !blueItemFound) {
+      $('#extra-details').text(`Mmmm, I don't seem to know yet how to classify that.`)
+    }
+
+    $('.classification-confirmation').hide()
+    $('.recycling-classification').show()
   })
 
   $('#next').bind('click', () => {
+    $('#result').empty()
     $('.recycling-classification').hide()
     $('.classification-content').show()
   })
-
-
-
-
 })
