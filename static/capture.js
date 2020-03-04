b0df46501983a501d153c08cd217466e370cf222
Ahmed <sultan_902002@yahoo.com>
2020-03-04 01:24:11 -0700

    display results to the html from the server response

--- static/capture.js
+++ static/capture.js
@@ -9,14 +9,26 @@ $(() => {
       .drawImage(video, 0, 0, canvas.width, canvas.height);
     let picture = canvas.toDataURL('image/png')
 
+    const isVowel = (string) => {
+      if (string.charAt(0) === 'a' || string.charAt(0) === 'e' || string.charAt(0) === 'i' || string.charAt(0) === 'o' || string.charAt(0) === 'u') {
+        return "an"
+      }
+      else return "a"
+    };
+
     $.post("/predict",
       {
         lable: "video capture",
         content: picture
       },
       function (data, status) {
-        result.value = status + ":" + data;
-        console.log(data)
+        
+        const result = `Is this ${isVowel(data)} ${data}?`
+
+        $('.classification-content').hide()
+        $('#result').text(result);
+
+        $('.classification-confirmation').show()
       })
   });
 })
