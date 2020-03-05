b7f838f943b037d29bccb73ba876534593fa8431
Ahmed <sultan_902002@yahoo.com>
2020-03-04 18:53:20 -0700

    add greenBitItems.js to classify where the item should go

--- static/blackBinItems.js
+++ static/blackBinItems.js
@@ -1,2 +1,12 @@
 export const blackBinItems = [
-]
+  'water ouzel',
+  'dipper',
+  'rubber-ring',
+  'coffee mug',
+  'golf ball',
+  'pencil sharpener',
+  'plastic bag',
+  'handbag',
+  'syringe',
+  'toothbrush'
+];
