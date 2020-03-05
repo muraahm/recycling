1aeaaf0e4c4fd0e6603669c344891208969a5f18
Ahmed <sultan_902002@yahoo.com>
2020-03-04 19:01:13 -0700

    refactor code to es6

--- static/capture.js
+++ static/capture.js
@@ -1,16 +1,20 @@
 $(() => {
 
-  $('.predict').bind('click', function () {
+  $('.predict').bind('click', () => {
 
     let canvas = document.createElement("canvas");
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     canvas.getContext('2d')
       .drawImage(video, 0, 0, canvas.width, canvas.height);
-    let picture = canvas.toDataURL('image/png')
+    let picture = canvas.toDataURL('image/png');
 
     const isVowel = (string) => {
-      if (string.charAt(0) === 'a' || string.charAt(0) === 'e' || string.charAt(0) === 'i' || string.charAt(0) === 'o' || string.charAt(0) === 'u') {
+      if (string.charAt(0) === 'a'
+        || string.charAt(0) === 'e'
+        || string.charAt(0) === 'i'
+        || string.charAt(0) === 'o'
+        || string.charAt(0) === 'u') {
         return "an"
       }
       else return "a"
@@ -21,13 +25,15 @@ $(() => {
         lable: "video capture",
         content: picture
       },
-      function (data, status) {
+       (data, status) => {
         const result = data.match(/^(\S+)\s(.*)/).slice(1);
         $('.classification-content').hide()
         $('#result').append(
-          `Is this ${isVowel(result[0])} <strong class="data">${result[0]}</strong> ?<br/>${(result[1] * 100).toFixed(2)}%`
+          `Is this ${isVowel(result[0])} 
+          <strong class="data">${result[0]}</strong> ?
+          <br/>${(result[1] * 100).toFixed(2)}%`
         )
         $('.classification-confirmation').show()
       })
-  });
-})
+  })
+});
